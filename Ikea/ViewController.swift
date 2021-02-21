63a8fba49b67a71f9bb196cf0855d1b06f2d2fc8
Houssam <houssam.naous1@gmail.com>
2021-02-21 18:43:37 +0100

    Added color to selected item

--- Ikea/ViewController.swift
+++ Ikea/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import ARKit
 
 
-class ViewController: UIViewController, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     let itemsArray: [String] = ["cup","vase", "boxing", "table"]
     @IBOutlet weak var itemsCollectionView: UICollectionView!
@@ -26,17 +26,31 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         
         // Set the collection view's data source
         self.itemsCollectionView.dataSource = self
+        
+        self.itemsCollectionView.delegate = self
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return itemsArray.count
     }
 
+    // Fills up cells content
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath ) as! itemCell
         cell.itemLabel.text = self.itemsArray[indexPath.row]
         return cell
     }
 
+    // Triggered whenever a certain cell is selected
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.green
+    }
+    
+    // When another item in the collection view is selected, this is called on the first item
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.orange
+    }
 }
 
