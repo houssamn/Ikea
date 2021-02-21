cca8a7da177b2f54c5e02866a6ed30bff2213707
Houssam <houssam.naous1@gmail.com>
2021-02-21 18:35:26 +0100

    Added scrollview at the bottom with ikea item names

--- Ikea/ViewController.swift
+++ Ikea/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UICollectionViewDataSource {
+
+    let itemsArray: [String] = ["cup","vase", "boxing", "table"]
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        
+        // Set the collection view's data source
+        self.itemsCollectionView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath ) as! itemCell
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
 
 }
 
---
+++ Ikea/itemCell.swift
@@ -0,0 +1,13 @@
+//
+//  itemCell.swift
+//  Ikea
+//
+//  Created by Houssam on 2/21/21.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+}
